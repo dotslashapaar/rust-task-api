12934091c74ae5849d28a342b6111421ec0e9857
dotslashapaar <apaarsaini007@gmail.com>
2026-06-22 01:06:19 +0530

    setting up pool

--- src/db/pool.rs
+++ src/db/pool.rs
@@ -1,1 +1,13 @@
+use std::time::Duration;
+use sqlx::{PgPool, postgres::PgPoolOptions};
 
+pub async fn create_pool(database_url: &str) -> Result<PgPool, sqlx::Error> {
+    PgPoolOptions::new()
+        .max_connections(10)
+        .min_connections(2)
+        .acquire_timeout(Duration::from_secs(5))
+        .max_lifetime(Duration::from_secs(30 * 60))
+        .idle_timeout(Duration::from_secs(10 * 60))
+        .connect(database_url)
+        .await
+}
